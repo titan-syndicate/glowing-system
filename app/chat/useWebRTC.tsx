ec98efe7b91a0f60a0deed8437c3a4345b0d137c
Chris Valarida <cvalarida@gmail.com>
2023-08-28 12:57:45 -0700

    prettier + stub event bits

--- app/chat/useWebRTC.tsx
+++ app/chat/useWebRTC.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 interface WebRTC {
   addIceCandidate: (candidate: string) => void;
@@ -18,39 +18,47 @@ interface WebRTC {
   sdpOffer: string;
   sendMessage: (message: string) => void;
 }
-export default function useWebRTC() : WebRTC {
+
+export default function useWebRTC(): WebRTC {
   const [peerConnection] = useState(new RTCPeerConnection());
-  const [dataChannel] = useState(peerConnection.createDataChannel('chat'));
-  const [dataChannelState, setDataChannelState] = useState<string>(dataChannel.readyState);
+  const [dataChannel] = useState(peerConnection.createDataChannel("chat"));
+  const [dataChannelState, setDataChannelState] = useState<string>(
+    dataChannel.readyState
+  );
   const [chatLog, setChatLog] = useState<string[]>([]);
   const [iceCandidates, setIceCandidates] = useState<string[]>([]);
-  const [sdpOffer, setSdpOffer] = useState<string>('');
-  const [sdpAnswer, setSdpAnswer] = useState<string>('');
+  const [sdpOffer, setSdpOffer] = useState<string>("");
+  const [sdpAnswer, setSdpAnswer] = useState<string>("");
 
   // #region SDP Offer/Answer
   const createSdpOffer = () => {
-    peerConnection.createOffer()
+    peerConnection
+      .createOffer()
       .then((offer) => peerConnection.setLocalDescription(offer))
       .then(() => {
         setSdpOffer(JSON.stringify(peerConnection.localDescription));
       });
-  }
+  };
 
   const handleRemoteOffer = (offer: string) => {
     const remoteOffer = new RTCSessionDescription(JSON.parse(offer));
-    peerConnection.setRemoteDescription(remoteOffer).then(() => {
-      return peerConnection.createAnswer();
-    }).then(answer => {
-      return peerConnection.setLocalDescription(answer);
-    }).then(() => {
-      setSdpAnswer(JSON.stringify(peerConnection.localDescription));
-    });
-  }
+    peerConnection
+      .setRemoteDescription(remoteOffer)
+      .then(() => {
+        return peerConnection.createAnswer();
+      })
+      .then((answer) => {
+        return peerConnection.setLocalDescription(answer);
+      })
+      .then(() => {
+        setSdpAnswer(JSON.stringify(peerConnection.localDescription));
+      });
+  };
 
   const handleRemoteAnswer = (answer: string) => {
     const remoteAnswer = new RTCSessionDescription(JSON.parse(answer));
     peerConnection.setRemoteDescription(remoteAnswer);
-  }
+  };
   // #endregion
 
   // #region Messaging
@@ -78,14 +86,17 @@ export default function useWebRTC() : WebRTC {
   // #region Ice Candidates
   peerConnection.onicecandidate = (event) => {
     if (event.candidate) {
-      setIceCandidates((prevLog) => [...prevLog, JSON.stringify(event.candidate)]);
+      setIceCandidates((prevCandidates) => [
+        ...prevCandidates,
+        JSON.stringify(event.candidate),
+      ]);
     }
   };
 
   const addIceCandidate = (iceCandidate: string) => {
     const candidate = new RTCIceCandidate(JSON.parse(iceCandidate));
     peerConnection.addIceCandidate(candidate);
-  }
+  };
   // #endregion
 
   return {
@@ -99,6 +110,5 @@ export default function useWebRTC() : WebRTC {
     sendMessage,
     sdpAnswer,
     sdpOffer,
-  }
+  };
 }
-
