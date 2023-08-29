fe4dc9062837d3b003cfc4463888684f9574b75c
Rian Fowler <rianf@me.com>
2023-08-29 00:00:21 +0000

    add some changes to the workflows

--- make-env.sh
+++ make-env.sh
@@ -6,7 +6,7 @@ set -e
 # Retrieve the secrets from vlt and write to .env file
 echo "DOCKER_USERNAME=$(vlt secrets get --plaintext DOCKER_USERNAME)" > .env
 echo "DOCKER_PASSWORD=$(vlt secrets get --plaintext DOCKER_PASSWORD)" >> .env
-echo "TS_GITHUB_TOKEN=$(vlt secrets get --plaintext TS_GITHUB_TOKEN)" >> .env
+echo "GITHUB_TOKEN=$(vlt secrets get --plaintext TS_GITHUB_TOKEN)" >> .env
 
 # Retrieve the KUBECONFIG_64 variable from vlt
 KUBECONFIG_64=$(vlt secrets get --plaintext KUBECONFIG_64)
