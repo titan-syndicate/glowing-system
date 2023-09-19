c9fee9c4b7af3d6b35f358f04f8f8797dea6c588
Rian Fowler <rianf@me.com>
2023-09-19 14:42:32 +0000

    fix image

--- app/(protected)/chat/layout.tsx
+++ app/(protected)/chat/layout.tsx
@@ -168,12 +168,10 @@ export default function Example() {
           <div className="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
           <a href="#">
             <span className="sr-only">Your profile</span>
-            <Image
+            <img
               className="h-8 w-8 rounded-full bg-gray-800"
               src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
               alt=""
-              width={32}
-              height={32}
             />
           </a>
         </div>
